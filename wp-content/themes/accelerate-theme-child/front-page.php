9ac8e76caec6239f64625cdd677c8031b70f4ffd
Kerri Varner <kerrivarner@protonmail.com>
2018-09-20 10:44:48 -0400

    added featured work custom fields to homepage

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -24,13 +24,37 @@ get_header(); ?>
 		</div><!-- .main-content -->
 	</div><!-- #primary -->
 
+<section class="featured-work">
+	<div class="site-content">
+		<h3>FEATURED WORK </h3>
+
+		<ul class="homepage-featured-work">
+			<?php query_posts('posts_per_page=3&post_type=case_studies&			orderby=title&order=ASC'); ?>
+				<?php while ( have_posts() ) : the_post(); 
+						$image_1 = get_field("image_1");
+						$size = "medium";
+						
+					?>
+			<li class="individual-featured-work">
+			  	<figure>
+					  <?php echo wp_get_attachment_image($image_1, $size); ?>
+				</figure>
+			  	<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+			</li>
+  		<?php endwhile; ?> 
+		
+		<?php wp_reset_query(); ?>
+		<ul>
+	</div>
+</section>
+
 <section class="recent-posts">
   <div class="site-content">
   	<div class="blog-post">
 	  <h4>From the Blog</h4>
 	  <?php query_posts('posts_per_page=1'); ?>
   			<?php while ( have_posts() ) : the_post(); ?>
-			  <h3><?php the_title(); ?></h3>
+			  <h2><?php the_title(); ?></h2>
        		  <?php the_excerpt(); ?> 
   			<?php endwhile; ?> 
 		<?php wp_reset_query(); ?>
