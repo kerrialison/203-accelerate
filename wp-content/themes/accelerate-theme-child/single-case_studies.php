d7c0139f288ccd4f0e920e85d11176033da3d6a7
Kerri Varner <kerrivarner@protonmail.com>
2018-09-20 10:48:23 -0400

    cleaned up case_studies template

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -14,41 +14,38 @@ get_header(); ?>
 		<div class="main-content" role="main">
 			<?php while ( have_posts() ) : the_post(); 
 
-			$services = get_field('services');
-			$client = get_field('client');
-			$link = get_field('site_link');
-			$image_1 = get_field('image_1');
-			$image_2 = get_field('image_2');
-			$image_3 = get_field('image_3');
-			?>
+					$services = get_field('services');
+					$client = get_field('client');
+					$link = get_field('site_link');
+					$image_1 = get_field('image_1');
+					$image_2 = get_field('image_2');
+					$image_3 = get_field('image_3');
+					?>
 
-		<article class="case-study">
-			<aside class="case-study-sidebar">
-				<h2> <?php the_title(); ?></h2>
-				<h5> <?php echo $services; ?> <h5>
-				<h6> <?php echo $client; ?> <h6>	
+				<article class="case-study">
+					<aside class="case-study-sidebar">
+						<h2> <?php the_title(); ?></h2>
+						<h5> <?php echo $services; ?> <h5>
+						<h6> <?php echo $client; ?> <h6>	
 
-				<?php the_content(); ?>
+						<?php the_content(); ?>
 
-				<p class="read-more-link"> <a href="<?php echo $link; ?>">Visit Live Site </a></p>
-			</aside>		
-			
-		<div class="case-study-images">
-		<?php if($image_1) {
-			echo wp_get_attachment_image( $image_1, $size );
-		} ?>
-		<?php if($image_2) {
-			echo wp_get_attachment_image( $image_2, $size );
-		} ?>
-		<?php if($image_3) {
-			echo wp_get_attachment_image( $image_3, $size );
-		} ?>
-
-		</div>
-		
-	
-	</article>
-			<?php endwhile; // end of the loop. ?>
+						<p class="read-more-link"> <a href="<?php echo $link; ?>">Visit Live Site </a></p>
+					</aside>		
+					
+				<div class="case-study-images">
+				<?php if($image_1) {
+					echo wp_get_attachment_image( $image_1, $size );
+				} ?>
+				<?php if($image_2) {
+					echo wp_get_attachment_image( $image_2, $size );
+				} ?>
+				<?php if($image_3) {
+					echo wp_get_attachment_image( $image_3, $size );
+				} ?>
+				</div>
+			</article>
+		<?php endwhile; // end of the loop. ?>
 		</div><!-- .main-content -->
 
 	</div><!-- #primary -->
